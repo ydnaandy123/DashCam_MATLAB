6fb66afaa1882ff87e784487283f5a3486272f99
andy <ydnaandy123@yahoo.com.tw>
2016-05-21 11:26:09 +0800

    url_data_by_matlab

--- url_data_by_matlab/url_data_by_matlab.m
+++ url_data_by_matlab/url_data_by_matlab.m
@@ -1,22 +1,20 @@
 clc;clear;close all;
+addpath('../lib');
 google_search = 'http://maps.google.com/cbk?output=json&cb_client=maps_sv&v=4&dm=1&pm=1&ph=1&hl=en&panoid=OdH7fZyrCUDrpEx4CvLLYA';
 matlab_results = parse_json(urlread(google_search));
-%disp(matlab_results{1}.Data.image_width)
-
-tile_width = str2double(matlab_results{1}.Data.tile_width);
-tile_height = str2double(matlab_results{1}.Data.tile_height);
-index_map = zeros(tile_height, tile_width);
-
-%imshow(index_map)
-
+% %disp(matlab_results{1}.Data.image_width)
+% tile_width = str2double(matlab_results{1}.Data.tile_width);
+% tile_height = str2double(matlab_results{1}.Data.tile_height);
+% index_map = zeros(tile_height, tile_width);
 depth_raw = matlab_results{1}.model.depth_map;
 
+%% Append '=' in order to make the length of the array a multiple of 4
 while mod(length(depth_raw), 4) ~= 0
     strcat(depth_raw, '=');
 end
-
+%% Replace '-' by '+' and '_' by '/'
 depth_raw = strrep(depth_raw, '-', '+');
 depth_raw = strrep(depth_raw, '_', '/');
-
+%% Decode and decompress data
 compressedDepthMapData = base64decode(depth_raw);
 decompressedDepthMap = zlibdecode(compressedDepthMapData);
--- url_data_by_python_export/import_python_output.m
+++ url_data_by_python_export/import_python_output.m
@@ -11,45 +11,49 @@ DepthMapIndices = reshape(DepthMapIndices, w, h);
 DepthMapIndices = DepthMapIndices';
 DepthMapIndices = reshape(DepthMapIndices, 1, w * h);
 %% Visual index map
-%depthMapVisual = reshape(DepthMapIndices, h, w);
+depthMapVisual = reshape(DepthMapIndices, h, w);
     %% Gray Image
-%     depthMapVisual = depthMapVisual / numPlanes;
-%     figure();
-%     imshow(depthMapVisual);
+    depthMapVisual_g = depthMapVisual / numPlanes;
+    figure();
+    imshow(depthMapVisual_g);
     %% Color Image
-%     randIndexColor = rand(3,numPlanes);
-%     depthMapVisual_r = zeros(h, w); depthMapVisual_g = zeros(h, w); depthMapVisual_b = zeros(h, w);
-%     for i=0:numPlanes-1
-%         depthMapVisual_r(depthMapVisual == i) =  randIndexColor(1, i+1);
-%         depthMapVisual_g(depthMapVisual == i) =  randIndexColor(2, i+1);
-%         depthMapVisual_b(depthMapVisual == i) =  randIndexColor(3, i+1);
-%     end
-%     depthMapVisual_color = cat(3, depthMapVisual_r, depthMapVisual_g, depthMapVisual_b);
-%     imshow(depthMapVisual_color);
+    randIndexColor = rand(3,numPlanes);
+    depthMapVisual_r = zeros(h, w); depthMapVisual_g = zeros(h, w); depthMapVisual_b = zeros(h, w);
+    for i=0:numPlanes-1
+        depthMapVisual_r(depthMapVisual == i) =  randIndexColor(1, i+1);
+        depthMapVisual_g(depthMapVisual == i) =  randIndexColor(2, i+1);
+        depthMapVisual_b(depthMapVisual == i) =  randIndexColor(3, i+1);
+    end
+    depthMapVisual_color = cat(3, depthMapVisual_r, depthMapVisual_g, depthMapVisual_b);
+    imshow(depthMapVisual_color);
 %% Create DepthMap
-y = 0:h-1;
-theta = (h - y - 0.5) / h * pi; sin_theta = sin(theta); cos_theta = cos(theta);
-x = 0:w-1;
-phi = (w - x - 0.5) / w * 2 * pi + pi/2; sin_phi = sin(phi); cos_phi = cos(phi);
-
-v = zeros(h,w,3); 
-v(:,:,1) = sin_theta' * cos_phi; v(:,:,2) = sin_theta' * sin_phi; v(:,:,3) = cos_theta' * ones(1,w);
-v = reshape(v, h*w, 3);
-depthMap = zeros(h, w); 
-depthMap(DepthMapIndices == 0) = inf;
-for i=1:numPlanes - 1
-    plane = DepthMapPlanes{i + 1};
-    d_vec = ones(w*h, 1)*plane.d;
-    dot_vec = v * [plane.nx; plane.ny; plane.nz];
-    t = abs(d_vec ./ dot_vec);
-    depthMap(DepthMapIndices == i) = t(DepthMapIndices == i);
-end
-figure();        
-imshow(depthMap /50 );
+    %% Emit Ray
+    y = 0:h-1;
+    theta = (h - y - 0.5) / h * pi; sin_theta = sin(theta); cos_theta = cos(theta);
+    x = 0:w-1;
+    phi = (w - x - 0.5) / w * 2 * pi + pi/2; sin_phi = sin(phi); cos_phi = cos(phi);
+    v = zeros(h,w,3); 
+    v(:,:,1) = sin_theta' * cos_phi; v(:,:,2) = sin_theta' * sin_phi; v(:,:,3) = cos_theta' * ones(1,w);
+    for i=1:3
+        figure();imshow(v(:,:,i))
+    end
+    v = reshape(v, h*w, 3);
+    %% Calculate depth
+    depthMap = zeros(h, w); 
+    depthMap(DepthMapIndices == 0) = inf;
+    for i=1:numPlanes - 1
+        plane = DepthMapPlanes{i + 1};
+        p_nomal = ones(w*h, 1)*plane.d;
+        depth = v * [plane.nx; plane.ny; plane.nz];
+        t = abs(p_nomal ./ depth);
+        depthMap(DepthMapIndices == i) = t(DepthMapIndices == i);
+    end
+    figure();        
+    imshow(depthMap /50 );
 %% Create Point Cloud
 numFaces = 100;
 [x,y,z] = sphere(numFaces);
-scatter3(x(:), y(:), z(:));
+%pcshow(x,y,z)
 % num_points = w*h;
 % positions_x = zeros(1, num_points);
 % positions_y = zeros(1, num_points);
